d53ea82cd62327b7c9691fc21def78b2b7a909f6
Juntao Qiu <juntao.qiu@gmail.com>
2019-03-04 22:57:47 +1100

    with react

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,51 @@ import ReactDOM from 'react-dom';
 
 import promiseService from './index';
 
+import { Machine, interpret } from 'xstate';
+
+const toggleMachine = Machine({
+  id: 'toggle',
+  initial: 'inactive',
+  states: {
+    inactive: {
+      on: { TOGGLE: 'active' }
+    },
+    active: {
+      on: { TOGGLE: 'inactive' }
+    }
+  }
+});
+
+class Toggle extends React.Component {
+  state = {
+    current: toggleMachine.initialState
+  };
+
+  service = interpret(toggleMachine).onTransition(current =>
+    this.setState({ current })
+  );
+
+  componentDidMount() {
+    this.service.start();
+  }
+
+  componentWillUnmount() {
+    this.service.stop();
+  }
+
+  render() {
+    const { current } = this.state;
+    const { send } = this.service;
+
+    console.log(current.value);
+    return (
+      <button onClick={() => send('TOGGLE')}>
+        {current.matches('inactive') ? 'Off' : 'On'}
+      </button>
+    );
+  }
+}
+
 class App extends React.Component {
     perform = () => {
         promiseService.send('RESOLVE');
@@ -17,4 +62,4 @@ class App extends React.Component {
 
 const app = document.querySelector('#app');
 
-ReactDOM.render(<App />, app);
+ReactDOM.render(<Toggle />, app);

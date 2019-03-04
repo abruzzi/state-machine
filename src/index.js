ccf6892e8bc3924069f408c5d893cd747ebc15cd
Juntao Qiu <juntao.qiu@gmail.com>
2019-03-04 22:44:17 +1100

    some draft of state machine

--- src/index.js
+++ src/index.js
@@ -1,5 +1,68 @@
 import { Machine, interpret } from 'xstate';
 
+const form = [
+  {
+    id: '1',
+    type: 'question',
+    name: 'type',
+    meta: {
+      question: 'Type?',
+      options: [
+        {label: 'Buy a property', value: 'buy'},
+        {label: 'Refinanacing', value: 'refinanacing'}
+      ]
+    }
+  },
+  {
+    id: '2',
+    type: 'input',
+    name: 'price',
+    meta: {
+      label: 'price'
+    }
+  }
+];
+
+const submission = {
+  type: 'buy',
+  price: '10000'
+};
+
+const event = Machine({
+    id: 'event',
+    initial: 'start',
+    states: {
+        start: {
+            on: {
+                BUY: 'buy',
+                REFINANCING: 'refinancing'
+            }
+        },
+        buy: {
+            on: {
+                STILL_LOOKING: 'looking',
+                PURCHASE: 'purchase'
+            }
+        },
+        looking: {
+        },
+        purchase: {
+        },
+        houseOrApartment: {
+          on: {
+            HOUSE: '',
+            APARTMENT: ''
+          }
+        },
+        new: {
+          type: 'final'
+        },
+        existing: {
+          type: 'final'
+        }
+    }
+})
+
 const promiseMachine = Machine({
   id: 'promise',
   initial: 'pending',

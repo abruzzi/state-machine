390e61a88405887ed8d8f81ca123344add0b37df
Juntao Qiu <juntao.qiu@gmail.com>
2019-03-06 20:47:08 +1100

    dynamic generate route

--- src/app.jsx
+++ src/app.jsx
@@ -122,6 +122,41 @@ function Topics ({ match }) {
   )
 }
 
+const Good = () => {
+  return <h2>Good</h2>
+}
+
+const NotBad = () => {
+  return (<h2>Not bad</h2>)
+}
+
+const QNA = ({history, match}) => {
+  const qna = {
+    name: 'howareyou',
+    question: 'How are you?',
+    answers: [
+      {label: 'Good', value: 'good', next: Good},
+      {label: 'Not bad', value: 'not-bad', next: NotBad},
+    ]
+  }
+  
+  const handler = (e) => {
+    history.push(`${match.path}/${e}`)
+  }
+
+  return (
+    <div>
+      <h2>{qna.question}</h2>
+      <ul>
+        {qna.answers.map((answer, index) => <li key={index} onClick={() => handler(answer.value)}>{answer.label}</li>)}
+      </ul>
+      {
+        qna.answers.map((answer, index) => <Route key={index} path={`${match.path}/${answer.value}`} component={answer.next}/>)
+      }
+    </div>
+  )
+}
+
 function Home () {
   return (
     <h1>
@@ -144,6 +179,7 @@ class App extends Component {
 
           <Route exact path='/' component={Home} />
           <Route path='/topics' component={Topics} />
+          <Route path='/qna' component={QNA} />
         </div>
       </Router>
     )
--- src/model.js
+++ src/model.js
@@ -1,5 +1,8 @@
+import _ from 'lodash';
+
 const state = {
   start: {
+    current: 'start',
     fields: [
       {
         type: 'radioGroup',
@@ -14,13 +17,8 @@ const state = {
     ]
   },
   address: {
+    current: 'address',
     fields: [
-      {
-        type: 'text',
-        meta: {
-          name: 'address'
-        }
-      },
       {
         type: 'radioGroup',
         meta: {
@@ -34,6 +32,50 @@ const state = {
     ]
   },
   done: {
-    
+    fields: []
   }
 }
+
+_.each(state, (value, key) => {
+  _.each(value.fields, )
+})
+
+const meta = [
+  {
+    name: 'start',
+    question: 'How are you?',
+    answers: [
+      { label: 'Good', value: 'good', link: 'address' },
+      { label: 'Not bad', value: 'mild', link: 'phone' }
+    ]
+  },
+  {
+    name: 'phone',
+    question: 'Local number',
+    answers: [
+      { label: 'Yes', value: 'yes', link: 'done' },
+      { label: 'No', value: 'no', link: 'done' }
+    ]
+  },
+  {
+    name: 'address',
+    question: 'Address?',
+    answers: [
+      { label: 'Local', value: 'local', link: 'done' },
+      { label: 'Oversea', value: 'oversea', link: 'done' }
+    ]
+  },
+  {
+    name: 'done',
+  }
+];
+
+meta.map((m) => (
+  <div>
+    <h2>{m.question}?</h2>
+    <ul>
+      {m.answers.map(a => <Link to={`${m.name}/${a.next}`}>a.label</Link>)}
+    </ul>
+    <Route path={} component={}></Route>
+  </div>
+));

ae44515fe291d5eddd7523ba01c3a34294dbf8aa
Juntao Qiu <juntao.qiu@gmail.com>
2019-03-09 09:52:32 +1100

    extract qnas to json

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -4,33 +4,14 @@ import makeQNA from '../utils/qna';
 import Good from './Good';
 import NotBad from './NotBad'
 
+import qnas from './qnas.json';
+
 const Local = () => <h2>Local</h2>
 const Oversea = () => <h2>Oversea</h2>
 const Secret = () => <h2>Secret</h2>
 
-export const HowAreYou = makeQNA('howareyou');
+const HowAreYou = makeQNA('howareyou');
 const Address = makeQNA('address');
-
-const qnas = [
-  {
-    name: 'address',
-    question: 'Where do you live?',
-    answers: [
-      {label: 'Local', value: 'local', route: 'local'},
-      {label: 'Oversea', value: 'oversea', route: 'oversea'},
-      {label: 'I dont want to tell', value: 'dont-tell', route: 'secret'},
-    ]
-  },
-  {
-    name: 'howareyou',
-    question: 'How are you?',
-    answers: [
-      {label: 'Good', value: 'good', route: 'good'},
-      {label: 'Not bad', value: 'not-bad', route: 'notbad'},
-      {label: 'Address', value: 'address', route: 'address'}
-    ]
-  }
-];
 
 const routeToCompMap = {
   local: Local,
@@ -54,3 +35,5 @@ const assemble = (qnas, routeToCompMap) => {
 const steps = assemble(qnas, routeToCompMap);
 
 export default steps;
+
+export {HowAreYou};
